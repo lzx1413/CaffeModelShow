4933161972bcfaf2a63dad688776f7e6f5d77f54
lzx1413 <lzx1413@live.cn>
2017-01-17 01:21:35 +0800

    update with opencv3.2 api

--- src/digitrecognition.cpp
+++ src/digitrecognition.cpp
@@ -12,28 +12,27 @@ DigitRecognition::~DigitRecognition()
 
 bool DigitRecognition::InitDNN(string prototxt,string caffemodel)
 {
+     cv::dnn::initModule();  //Required if OpenCV is built as static libs
     dnn_prototxt_path = prototxt;
     dnn_caffemodel_path = caffemodel;
-    cv::Ptr<dnn::Importer> importer;//创建caffe model的导入器
     try
     {
-        importer = dnn::createCaffeImporter(this->dnn_prototxt_path,this->dnn_caffemodel_path);//导入lenet的模型和参数
+         net = dnn::readNetFromCaffe(dnn_prototxt_path, dnn_caffemodel_path);
     }
     catch(const cv::Exception &err)
     {
         cerr<<err.msg<<endl;
     }
-    if(!importer)
+    if (net.empty())
     {
         std::cerr << "Can't load network by using the following files: " << std::endl;
-        std::cerr << "prototxt:   " << this->dnn_prototxt_path << std::endl;
-        std::cerr << "caffemodel: " << this->dnn_caffemodel_path << std::endl;
-
+        std::cerr << "prototxt:   " << dnn_prototxt_path<< std::endl;
+        std::cerr << "caffemodel: " << dnn_caffemodel_path << std::endl;
+        return false;
     }
-    importer->populateNet(this->net);//初始化神经网络
-    importer.release();
-    std::cout<<"finish init dnn model"<<std::endl;
-    return true;
+    else
+        std::cout<<"finish init dnn model"<<std::endl;
+        return true;
 }
 
 bool DigitRecognition::predict(Mat &img, int &classId, double &classProb)
@@ -43,7 +42,7 @@ bool DigitRecognition::predict(Mat &img, int &classId, double &classProb)
     resize(img,img,Size(28,28));//网络只接受28*28图像
     if(img.channels() ==3)//网络只接受1通道图像
         cvtColor(img,img,CV_BGR2GRAY);
-    dnn::Blob inputBlob = dnn::Blob(img);
+    dnn::Blob inputBlob = dnn::Blob::fromImages(img);
     net.setBlob(".data",inputBlob);//设置输入blob
     try{
     net.forward();//网络前向计算
@@ -53,7 +52,7 @@ bool DigitRecognition::predict(Mat &img, int &classId, double &classProb)
         return false;
 
     }
-    dnn::Blob prob = net.getBlob("loss");//获得概率结果
+    dnn::Blob prob = net.getBlob("prob");//获得概率结果
     getMaxClass(prob,&classId,&classProb);
     return true;
 
